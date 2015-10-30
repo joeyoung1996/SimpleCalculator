b0f020a7fd0fc512570e4be22e6663966ecc13e8
Joseph Young <youngjl@student.swosu.edu>
2015-10-30 15:41:11 -0500

    Got data validation working and reorganized the code into 3 methods.

--- Calculator.java
+++ Calculator.java
@@ -1,115 +1,90 @@
-import java.util.Scanner;
+import java.util.*;
 import java.lang.String;
 public class Calculator 
 {
 	public static void main(String[] args) 
 	{
-		double firstNumber = 0;
-		double secondNumber = 0;
-		double answer = 1;
-		boolean isProgramDone = false;
-		boolean wasOperatorChosen = false;
-		boolean isConfirmationDone = false;
-      Scanner input = new Scanner(System.in);
-		
+		double firstNumber;
+		double secondNumber;
 		String operation = null;
-		String confirmation;
-		String operationWord = null;
+		Scanner input = new Scanner(System.in);
 		
-		while (isProgramDone = false)
-		{
-			
-		System.out.println("Please enter the first number in your calculation: ");
-		try
-		{
-			
-			firstNumber = input.nextDouble();
-			System.out.println("Great, now enter the secondnumber: ");
-			secondNumber = input.nextDouble();
-		}	
-			
-			
-		catch(Exception e)
-		{
-			
-			System.out.println("Please Enter a numeric value");
-			
-		}
-		
-		System.out.println("Now, enter the operation to be performed (ie + - * /) : ");
-		operation = input.next();
-		
-		
-		System.out.println("You have entered: " + firstNumber + " " + operation + " " + secondNumber + ". ");
-		System.out.println("Is this correct? Y/N");
-		confirmation = input.next();
+		System.out.println("Please enter the first number: ");
+		firstNumber = doubleValidation();
+		System.out.println("Please enter the second number: ");
+		secondNumber = doubleValidation();
+		System.out.println(calculatorIn(firstNumber, secondNumber));
+
+	}
 	
-		while(isConfirmationDone = false)
+	public static double calculatorIn(double firstNumber, double secondNumber)
+	{
+		Scanner input = new Scanner(System.in);
+		double answer= 0;
+		String operation;
+		boolean isLoopDone = false;
+		
+		while (!isLoopDone)
 		{
-		if (confirmation.equalsIgnoreCase("Y"))
-		{
-			isConfirmationDone = true;
-			while(wasOperatorChosen == false)
-			{
+			System.out.println("Please enter the operation to perform: ");
+			operation = input.next();
 			if (operation.equals("+"))
 			{
 				answer = firstNumber + secondNumber;
-				operationWord = "sum";
-				wasOperatorChosen = true;
+				isLoopDone = true;
 			}
 			else if (operation.equals("-"))
 			{
 				answer = firstNumber - secondNumber;
-				operationWord = "difference";
-				wasOperatorChosen = true;
+				isLoopDone = true;
 			}
-			
+		
 			else if (operation.equals("*"))
 			{
 				answer = firstNumber * secondNumber;
-				operationWord = "product";
-				wasOperatorChosen = true;
+				isLoopDone = true;
 			}
 			else if (operation.equals("/"))
 			{
 				answer = firstNumber / secondNumber;
-				operationWord = "quotient";
-				wasOperatorChosen = true;
+				isLoopDone = true;
 			}
 			else
 			{
-				System.out.println("Please enter one of the operator symbols ( + - * /");
+				System.out.println("Please enter a vaild operation symbol: \n +  -  *  /\n");
+				isLoopDone = false;
 			}
-		}
-		}
-		else if (confirmation.equalsIgnoreCase("N"))
-		{
-			System.out.println("Please correct the calculation. ");
-			isProgramDone = false;
-			wasOperatorChosen = false;
-			isConfirmationDone = false;
-		}
-		else
-		{
-			System.out.println("Please enter a Y or N.");
-		}
-		System.out.println("The " + operationWord + " of " + firstNumber + " " + operation + " " + secondNumber + " = " + answer);
-		System.out.println("Would you like to continue? Y/N");
-		confirmation = input.next();
-		if (confirmation.equals("Y"))
-		{
-			isProgramDone = false;
-		}
-		else
-		{
-			System.out.println("Goodbye!");
-			isProgramDone = true;
-		}
-		
-		}
 		
 	}
+		return answer;
+	}
+	
+	
+	
+	public static double doubleValidation()
+	{
+		double inputDouble = 0;
+		boolean isLoopDone = false;
+		
+		while (!isLoopDone)
+		{
+			try
+			{
+				Scanner input = new Scanner(System.in);
+				inputDouble = input.nextDouble();
+				isLoopDone = true;
+			}
+				catch(InputMismatchException e)
+				{
+					System.out.println("Data input error. \n\nPlease enter a numeric value: ");
+				}
+			
+		}
+		
+		return inputDouble;
+	}
+	
+	
 }
-	}
 
 
